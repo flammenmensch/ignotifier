f57b8e3d62044e009fd1a433c8fa0208ccbc0595
Alexey Protasov <a.protasov@corp.mail.ru>
2015-05-21 17:05:07 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var http = require('http');
+var request = require('request');
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var WebSocketServer = require('ws').Server;
+
+var config = require('./lib/config');
+var apiRouter = require('./lib/routes/api');
+var callbackRouter = require('./lib/routes/callback');
+
+var app = express();
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.use('/api', apiRouter);
+app.use('/callback', callbackRouter);
+
+app.use(function(err, req, res, next) {
+  res.status(500).json({ error: true, message: err.message });
+});
+
+var server = http.createServer(app);
+var wss = new WebSocketServer({ server: server });
+
+wss.on('connection', function (ws) {
+
+
+  ws.on('close', function () {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(config.port, function() {
+  console.log('Server started');
+});
---
+++ public/admin/scripts/app.js
@@ -0,0 +1,85 @@
+"use strict";
+
+var app = angular.module('app', []);
+
+app.factory('SubscriptionService', function($http) {
+  function list() {
+    return $http.get('/api/subscription').then(function(response) {
+      return response.data.data;
+    });
+  }
+
+  function subscribe(tag) {
+    return $http.post('/api/subscription', { tag: tag });
+  }
+
+  function unsubscribe(id) {
+    return $http.delete('/api/subscription', { id: id });
+  }
+
+  function clear() {
+    return $http.post('/api/subscription/removeall');
+  }
+
+  return {
+    list: list,
+    subscribe: subscribe,
+    unsubscribe: unsubscribe,
+    clear: clear
+  };
+});
+
+app.controller('SubscriptionListCtrl', function($rootScope, SubscriptionService) {
+  var self = this;
+
+  this.subscriptions = [];
+  this.busy = false;
+
+  this.listSubscriptions = function() {
+    self.busy = true;
+
+    SubscriptionService.list()
+      .then(function(items) {
+        self.subscriptions = items;
+      })
+      .finally(function() {
+        self.busy = false;
+      });
+  };
+
+  this.unsubscribe = function(id) {
+    self.busy = true;
+
+    SubscriptionService.unsubscribe(id)
+      .then(function() {
+        self.listSubscriptions();
+      })
+      .finally(function() {
+        self.busy = false;
+      });
+  };
+
+  $rootScope.$on('app:subscriptions-changed', this.listSubscriptions);
+
+  this.listSubscriptions();
+});
+
+app.controller('FormCtrl', function($rootScope, SubscriptionService) {
+  var self = this;
+
+  this.busy = false;
+  this.tagName = '';
+
+  this.createSubscription = function() {
+    self.busy = true;
+
+    SubscriptionService.subscribe(self.tagName)
+      .then(function() {
+        $rootScope.$broadcast('app:subscriptions-changed');
+      })
+      .finally(function() {
+        self.busy = false;
+        self.tagName = '';
+      });
+  };
+});
